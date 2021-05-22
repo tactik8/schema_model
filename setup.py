518b5d4fc0cc576a8b03dadc14addd00a3049265
tactik8 <>
2021-05-22 15:55:49 +0000

    go

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="kraken-record",
-    version="0.0.2",
+    version="0.0.4",
     description="Kraken schema helper",
     long_description=README,
     long_description_content_type="text/markdown",
